65339441e90af40ef16ca40c155787649d696437
Aurélien <caruge.aurelien.wd@gmail.com>
2024-05-04 13:04:24 +0200

    exit
    Add Exit Method

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    public bool UNITY_EDITOR { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,15 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        SceneManager.LoadScene(0);
+#else
+        Application.Quit();
+#endif
     }
 
     // Update is called once per frame
